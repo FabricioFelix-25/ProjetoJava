83acb2ad418a3ab35e2e1caab8d4b5b05186c5f8
Fabricio Felix <felixfabricio115@gmail.com>
2024-08-28 14:21:26 -0300

    inicio de fato

---
+++ src/Filme.java
@@ -0,0 +1,9 @@
+public class Filme {
+        static String nome;
+        int anoDeLancamento;
+        boolean incluidoNoPlano;
+        double avaliacao;
+        int totalDeAvaliacoes;
+        int duracaoEmMinutos;
+    }
+
