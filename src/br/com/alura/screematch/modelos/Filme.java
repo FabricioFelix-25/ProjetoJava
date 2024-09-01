5a6bb37efb6c2d6d231ef46f02f32caabeee4e30
Fabricio Felix <felixfabricio115@gmail.com>
2024-09-01 14:55:49 -0300

    getter setter

--- src/br/com/alura/screematch/modelos/Filme.java
+++ src/br/com/alura/screematch/modelos/Filme.java
@@ -1,16 +1,55 @@
 package br.com.alura.screematch.modelos;
 
 public class Filme {
-        public String nome;
-        int anoDeLancamento;
-        boolean incluidoNoPlano;
+        private String nome;
+        private int anoDeLancamento;
+        private boolean incluidoNoPlano;
         private double somaDasAvaliacoes;
         private int totalDeAvaliacoes;
-        int duracaoEmMinutos;
+        private int duracaoEmMinutos;
+
+        //getter e set//
+
+
+        public String getNome() {
+                return nome;
+        }
+
+        public int getAnoDeLancamento() {
+                return anoDeLancamento;
+        }
+
+        public boolean isIncluidoNoPlano() {
+                return incluidoNoPlano;
+        }
+
+        public int getDuracaoEmMinutos() {
+                return duracaoEmMinutos;
+        }
 
         public int getTotalDeAvaliacoes(){
                 return totalDeAvaliacoes;
         }
+
+        public void setNome(String nome) {
+                this.nome = nome;
+        }
+
+        public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+                this.duracaoEmMinutos = duracaoEmMinutos;
+        }
+
+        public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+                this.incluidoNoPlano = incluidoNoPlano;
+        }
+
+        public void setAnoDeLancamento(int anoDeLancamento) {
+                this.anoDeLancamento = anoDeLancamento;
+        }
+
+
+
+
 
         public void exibeFichaTecnica(){
                 System.out.println("Nome do filme: " + nome);
