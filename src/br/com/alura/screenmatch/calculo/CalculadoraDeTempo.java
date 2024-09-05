dda78e8c1e7cb52522fb5ec64dbabfa67199f835
Fabricio Felix <felixfabricio115@gmail.com>
2024-09-05 14:03:55 -0300

    limpeza de codigo

--- src/br/com/alura/screenmatch/calculo/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculo/CalculadoraDeTempo.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch.calculo;
 
 import br.com.alura.screematch.modelos.Filme;
+import br.com.alura.screematch.modelos.Serie;
+import br.com.alura.screematch.modelos.Titulo;
 
 public class CalculadoraDeTempo {
     private int tempoTotal = 0;
@@ -10,8 +12,16 @@ public class CalculadoraDeTempo {
         return this.tempoTotal;
     }
 
-    public void inclui(Filme f){
-        tempoTotal += f.getDuracaoEmMinutos();
+//    public void inclui(Filme f){
+//        tempoTotal += f.getDuracaoEmMinutos();
+//    }
+//    public void inclui(Serie s){
+//        tempoTotal += s.getDuracaoEmMinutos();
+//    }
+
+    public void inclui(Titulo titulo){
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
     }
 
 }
