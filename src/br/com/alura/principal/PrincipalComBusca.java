7dbbc353da0986d8ab94a03a50ec4d17497fd116
Fabricio Felix <felixfabricio115@gmail.com>
2024-10-21 20:02:07 -0300

    uso de Scanner

--- src/br/com/alura/principal/PrincipalComBusca.java
+++ src/br/com/alura/principal/PrincipalComBusca.java
@@ -1,4 +1,4 @@
-﻿package br.com.alura.principal;
+package br.com.alura.principal;
 
 
 import java.io.IOException;
@@ -6,15 +6,24 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
-    public static void main (String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "apí";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=6585022c"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+
+
     }
 }
